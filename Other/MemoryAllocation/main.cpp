5b9f9f7ca02d66118e9ffdfb95c6763cb8b635c0
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2017-04-23 12:42:58 +0100

    last commit before the email.

--- Other/MemoryAllocation/main.cpp
+++ Other/MemoryAllocation/main.cpp
@@ -2,32 +2,47 @@
 #include "Object.h"
 #include "OverloadedObject.h"
 
+int objectMain();
+int overloadedObjectMain();
+
 int main() {
 
-    Object* object = new Object();
-
-    object->setNumber(5);
-    std::cout << "Hello, object number: " << object->getNumber() << std::endl;
-    /*object->~Object();
-    std::cout << "Hello, object number: " << object->getNumber() << std::endl;
-    new (object) Object();
-    std::cout << "Hello, object number: " << object->getNumber() << std::endl;*/
-    delete object;
-    std::cout << "Hello, object number: " << object->getNumber() << std::endl;
-
-    std::cout << "--------------------------------" << std::endl;
-
-    OverloadedObject* overloadedObject = new OverloadedObject;
-
-    overloadedObject->setNumber(5);
-    std::cout << overloadedObject->getNumber() << std::endl;
-    /*overloadedObject->~OverloadedObject();
-    std::cout << overloadedObject->getNumber() << std::endl;
-    new (overloadedObject) OverloadedObject(  );
-    std::cout << "Hello, overloadedObject number: " <<
-                  overloadedObject->getNumber() << std::endl;*/
-    delete overloadedObject;
-    std::cout << overloadedObject->getNumber() << std::endl;
+    objectMain();
+    overloadedObjectMain();
 
     return 0;
 }
+
+int objectMain() {
+
+    Object* object = new Object();
+
+    std::cout << "--------------- objectMain start" << std::endl;
+    object->setNumber(5);
+    std::cout << "Hello, object number: " << object->getNumber() << std::endl;
+
+    delete object;
+    std::cout << "Hello, object number: " << object->getNumber() << std::endl;
+
+    std::cout << "--------------- objectMain end" << std::endl;
+
+    return 0;
+}
+
+int overloadedObjectMain() {
+
+    std::cout << "--------------- overloadedObjectMain start" << std::endl;
+
+    OverloadedObject* overloadedObject = new OverloadedObject;
+    overloadedObject->setNumber(5);
+    std::cout << "Hello, object number: " << overloadedObject->getNumber() << std::endl;
+
+    delete overloadedObject;
+    std::cout << "Hello, object number: " << overloadedObject->getNumber() << std::endl;
+
+    std::cout << "--------------- overloadedObjectMain end" << std::endl;
+
+    return 0;
+}
+
+
